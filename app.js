b524280fc5bbc0387b1b677da083698a322ec46a
towbee98 <tobiemma200@gmail.com>
2021-05-13 04:13:45 +0100

    refactor:add the landing page

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ app.use(express.json()); //this middleware enables us to access the validation d
 const router = require("./routes");
 
 //Route Handler for getting all data and creating new data
-app.use("/api/users", router); //this middleware handles the specified routes
 
+app.use("/api/users", router); //this middleware handles the specified routes
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome this is the Zuri Crud App Task for Nodejs Intern",
+  });
+});
 //Creates a route handler to handle all routes not defined in the above middleware
 app.all("*", (req, res) => {
   res.status(404).json({
